2a16a33fc0e172195dd99639b2a2dd9a678032c0
pon-k <ignoretsu@gmail.com>
2025-12-05 00:52:52 +1300

    put test code into functions

--- t8_replay_get.py
+++ t8_replay_get.py
@@ -2,19 +2,38 @@ import requests
 import json
 import logging
 import traceback
+import logging
 
-headers = {"Accept-Encoding": "gzip, deflate, br"}
+logging.basicConfig(
+                    filename="t8data.log",
+                    format='%(asctime)s %(message)s',
+                    filemode='w')
+
 replays_url = "https://wank.wavu.wiki/api/replays"
 
-try:
-    response = requests.get(replays_url)
-    if response.ok:
-        replay_data_raw = response.json()
-        replay_data_clean = json.dumps(replay_data_raw, indent=4)
-        print(replay_data_clean)
-        
-    else:
-        print(response.status_code)
+# HTTP request func that returns JSON from wavu API
+ 
+def get_data(before_par=1764825684):
+    try:
+        response = requests.get(replays_url, params={"before": before_par})
+        if response.ok:
+            replay_data_raw = response.json()
+            replay_data_clean = json.dumps(replay_data_raw, indent=4)
+            logging.info(f"{response.status_code}. Logging replays from {before_par}... ")
+            print(replay_data_clean)
+            return replay_data_raw
+            
+        else:            
+            logging.error(response.status_code)
+            return False
 
-except Exception as e:
-    logging.error(traceback.format_exc())
+    except Exception as e:
+        logging.error(traceback.format_exc())
+        return False
+
+def loop_func(limit_par, before):
+    while before >= limit_par:
+        data_fetch = get_data(before)
+        if data_fetch:
+
+            before -= 700
