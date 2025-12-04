a57951a14ac29226bc3e3190f758b264aab4a7b1
pon-k <ignoretsu@gmail.com>
2025-12-05 12:28:14 +1300

    finished loop func, added logfiles to gitignore

--- t8_replay_get.py
+++ t8_replay_get.py
@@ -4,7 +4,7 @@ import logging
 import traceback
 import logging
 
-logging.basicConfig(
+logging.basicConfig(level=logging.INFO,
                     filename="t8data.log",
                     format='%(asctime)s %(message)s',
                     filemode='w')
@@ -18,9 +18,8 @@ def get_data(before_par=1764825684):
         response = requests.get(replays_url, params={"before": before_par})
         if response.ok:
             replay_data_raw = response.json()
-            replay_data_clean = json.dumps(replay_data_raw, indent=4)
+            #replay_data_clean = json.dumps(replay_data_raw, indent=4)
             logging.info(f"{response.status_code}. Logging replays from {before_par}... ")
-            print(replay_data_clean)
             return replay_data_raw
             
         else:            
@@ -31,9 +30,24 @@ def get_data(before_par=1764825684):
         logging.error(traceback.format_exc())
         return False
 
+# Loop that collects replays from specified period
+
 def loop_func(limit_par, before):
+    loop_limit = 0
     while before >= limit_par:
         data_fetch = get_data(before)
         if data_fetch:
-
+            with open("t8data_dump", "w") as f:
+                json.dump(data_fetch, f)
             before -= 700
+        else:
+            if loop_limit < 5:
+                loop_limit += 1
+                logging.error("Data fetch failed, retrying...")
+            else:
+                logging.error("Max retries reached, exiting loop.")
+                break    
+    
+if __name__ == "__main__":
+    loop_func(1654041600, 1764825684)
+    logging.info("Data fetch complete.")
